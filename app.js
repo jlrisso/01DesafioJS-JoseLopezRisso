ad70d599c31f1680599bc5c06fa48f8e8d9e19f2
jlrissom <jal.risso@gmail.com>
2022-07-20 01:56:04 -0300

    1.0

---
+++ app.js
@@ -0,0 +1,30 @@
+
+/**
+ * 
+ * @Author: JOSE LOPEZ RISSO 
+ * @Email: jal.risso@gmail.com
+ * @Date: 18-07-2022 
+ * @License: GPL
+ * 
+ * 
+ **/
+
+
+
+let myNumber = Number(prompt("Escribi cualquier numero distinto de 0: "));
+
+while(myNumber){  //Mientras sea un numero -> EJECUTAR 
+    
+    //Checkear condicion de PAR
+    if (myNumber %2 === 0) console.log("El numero: "+myNumber+" es par");
+    else console.log("El numero: "+myNumber+" no es par");
+
+    //Checkear condicion de POSITIVO
+    if (myNumber > 0) console.log("El numero: "+myNumber+" es positivo (+)");
+    else console.log("El numero: "+myNumber+" es negativo (-)");
+
+    //Pedir numero nuevamente
+    myNumber = Number(prompt("Escribi un numero: "));
+
+}
+
